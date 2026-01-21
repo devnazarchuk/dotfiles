d44e64deec7649a31cf22f4c4c062c57b45d1715
devnazarchuk <devnazarchuk@gmail.com>
2026-01-21 13:16:40 +0100

    v02 minecraft theme + bluetooth + librepods

--- config/i3/scripts/lock.sh
+++ config/i3/scripts/lock.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
-# Lock screen by redirecting to LightDM Greeter (Blocking version for xss-lock)
-# Dependencies: lightdm, dbus-monitor
+# Lock the session by switching to the SDDM Greeter (MineSDDM)
 
-# 1. Trigger the lock (switch to greeter)
-dm-tool lock
+# This will show the actual SDDM login screen with the Minecraft theme
+dbus-send --system --print-reply --dest=org.freedesktop.DisplayManager /org/freedesktop/DisplayManager/Seat0 org.freedesktop.DisplayManager.Seat.SwitchToGreeter
 
-# 2. Identify current session
-SESSION_ID=${XDG_SESSION_ID:-$(loginctl | grep "$USER" | awk '{print $1}' | head -n 1)}
-
-# 3. Wait until the session becomes active again
-# We listen for PropertiesChanged on our session and check the 'Active' property
-dbus-monitor --system "type='signal',interface='org.freedesktop.DBus.Properties',member='PropertiesChanged',path='/org/freedesktop/login1/session/_${SESSION_ID}'" | while read -r line; do
-    if loginctl show-session "$SESSION_ID" -p Active --value | grep -q "yes"; then
-        break
-    fi
-done
-
-# If session is active, exit script so xss-lock knows we are done
 exit 0
