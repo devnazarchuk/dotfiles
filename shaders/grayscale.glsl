ae4f417e4e53018adf628cdd3ec7141d0ee9cc02
Artem Nazarchuk <devnazarchuk@gmail.com>
2026-03-17 23:47:40 +0100

    Refactor: Remove omarchy systemd user units, update hyprctl shader check to use `jq`, and modernize GLSL shader syntax.

--- shaders/grayscale.glsl
+++ shaders/grayscale.glsl
@@ -1,9 +1,11 @@
+#version 300 es
 precision mediump float;
-varying vec2 v_texcoord;
+in vec2 v_texcoord;
 uniform sampler2D tex;
+out vec4 fragColor;
 
 void main() {
-    vec4 pix = texture2D(tex, v_texcoord);
+    vec4 pix = texture(tex, v_texcoord);
     float grayscale = dot(pix.rgb, vec3(0.299, 0.587, 0.114));
-    gl_FragColor = vec4(vec3(grayscale), pix.a);
+    fragColor = vec4(vec3(grayscale), pix.a);
 }
